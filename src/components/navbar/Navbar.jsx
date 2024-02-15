cf51dd8a1dc201fc2e4e3adf1bbc28cb0e3a7ee3
MatiasSimonLuduena <matipax04@gmail.com>
2024-02-15 14:51:00 -0300

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import "./navbar.css"
+import { useState } from "react"
+
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons"
+
+const Navbar = () => {
+    const [drawer, setDrawer] = useState(null);
+
+    return (
+        <>
+            <nav className="navbar">
+                <span className="logo">Matías Simón Ludueña</span>
+                <div className="menu">
+                    <button>Home</button>
+                    <button>About me</button>
+                    <button>Projects</button>
+                    <button>Contact me</button>
+                </div>
+                <button className="button_menu-burger" onClick={() => setDrawer("left")}>
+                    <FontAwesomeIcon icon={faBars}/>
+                </button>
+            </nav>
+            <div className={`container_menu-burger ${
+                !drawer ? "" : drawer === "left" ? "menu-left" : "menu-right"
+            }`}
+                onClick={() => setDrawer("right")}
+            >
+                <div className="menu-burger" onClick={e => e.stopPropagation()}>
+                    <FontAwesomeIcon icon={faX} onClick={() => setDrawer("right")}/>
+                    <button>Home</button>
+                    <button>About me</button>
+                    <button>Projects</button>
+                    <button>Contact me</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
