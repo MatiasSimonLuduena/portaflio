8199e5b104db87f9484a7edecb6d13ed990ff096
MatiasSimonLuduena <matipax04@gmail.com>
2024-02-16 16:31:25 -0300

    home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,26 @@
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-top">
+        <h1>Matias Simon Ludueña</h1>
+        <div className="home-top_line"></div>
+        <span>Full stack MERN developer.</span>
+      </div>
+      <div className="home-main">
+        <p>
+          My name is Matias Ludueña, I&apos;m a full stack developer. I master
+          both the frontend and the backend using the MERN (MongoDB, ExpressJS,
+          ReactJS and NodeJS) technologies. I was born in Argentina, I am
+          currently 22 years old and I have been making web applications since I
+          was 19 years.
+          <br />
+          <br />I have developed many types of websites...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,42 +1,44 @@
-import "./navbar.css"
-import { useState } from "react"
+import './navbar.css';
+import { useState } from 'react';
 
 // icons
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars, faX } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-    const [drawer, setDrawer] = useState(null);
+  const [drawer, setDrawer] = useState(null);
 
-    return (
-        <>
-            <nav className="navbar">
-                <span className="logo">Matías Simón Ludueña</span>
-                <div className="menu">
-                    <button>Home</button>
-                    <button>About me</button>
-                    <button>Projects</button>
-                    <button>Contact me</button>
-                </div>
-                <button className="button_menu-burger" onClick={() => setDrawer("left")}>
-                    <FontAwesomeIcon icon={faBars}/>
-                </button>
-            </nav>
-            <div className={`container_menu-burger ${
-                !drawer ? "" : drawer === "left" ? "menu-left" : "menu-right"
-            }`}
-                onClick={() => setDrawer("right")}
-            >
-                <div className="menu-burger" onClick={e => e.stopPropagation()}>
-                    <FontAwesomeIcon icon={faX} onClick={() => setDrawer("right")}/>
-                    <button>Home</button>
-                    <button>About me</button>
-                    <button>Projects</button>
-                    <button>Contact me</button>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <nav className="navbar">
+        <span className="logo">Matías Simón Ludueña</span>
+        <div className="menu">
+          <button>Home</button>
+          <button>Projects</button>
+          <button>Contact me</button>
+        </div>
+        <button
+          className="button_menu-burger"
+          onClick={() => setDrawer('left')}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </nav>
+      <div
+        className={`container_menu-burger ${
+          !drawer ? '' : drawer === 'left' ? 'menu-left' : 'menu-right'
+        }`}
+        onClick={() => setDrawer('right')}
+      >
+        <div className="menu-burger" onClick={(e) => e.stopPropagation()}>
+          <FontAwesomeIcon icon={faX} onClick={() => setDrawer('right')} />
+          <button>Home</button>
+          <button>Projects</button>
+          <button>Contact me</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
