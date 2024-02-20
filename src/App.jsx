d8c32145486c76daac004d525b9681160d17ee35
MatiasSimonLuduena <matipax04@gmail.com>
2024-02-20 14:10:45 -0300

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/Navbar"
 import Home from "./components/home/Home"
 import Projects from "./components/projects/Projects"
+import Contact from "./components/contact/Contact"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Home/>
       <Projects/>
+      <Contact/>
     </>
   )
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,42 @@
+import './contact.css';
+
+const socialsmedia = [
+  { title: 'Whatsapp', image: 'icons/whatsapp.png' },
+  { title: 'Instagram', image: 'icons/instagram.png' },
+  { title: 'LinkedIn', image: 'icons/linkedin.png' },
+  { title: 'GitHub', image: 'icons/github.png' },
+];
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h3>Do you want to contact me?</h3>
+      <div className="contact-container">
+        <div className="contact-form">
+          <span>Send me a message</span>
+          <form>
+            <input type="text" placeholder="Your name" />
+            <input type="email" placeholder="Your email" />
+            <input type="number" placeholder="Your Whatsapp number" />
+            <textarea placeholder="Your message"></textarea>
+          </form>
+          <button>
+            Send message
+            <div className="button-line"></div>
+          </button>
+        </div>
+        <div className="contact-socialmedia">
+          <span>Follow me and contact me on my social networks</span>
+          <div className="socialsmedia">
+            {socialsmedia.map((item, i) => (
+              <img key={i} alt={item.title} src={item.image} />
+            ))}
+          </div>
+          <p className='contact-email'>matiassimonluduena@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
